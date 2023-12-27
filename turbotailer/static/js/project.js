fef3e66f6f96fc575430bdbb034887e271064540
Emil Nielsen <hej@emilnielsen.com>
2023-12-27 22:04:05 +0700

    Style header

--- turbotailer/static/js/project.js
+++ turbotailer/static/js/project.js
@@ -1,1 +1,16 @@
 import '../sass/project.scss';
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Open menu mobile
+    var button = document.getElementById('dropdownButton');
+    var dropdown = document.getElementById('dropdownMenu');
+
+    button.addEventListener('click', function() {
+        var isHidden = dropdown.hasAttribute('hidden');
+        if (isHidden) {
+            dropdown.removeAttribute('hidden');
+        } else {
+            dropdown.setAttribute('hidden', '');
+        }
+    });
+});
