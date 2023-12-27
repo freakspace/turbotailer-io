07589dd17180c2d20b11f14ce0b56b577c32dbef
Emil Nielsen <hej@emilnielsen.com>
2023-12-27 08:20:49 +0700

    Add stage configuration

---
+++ config/settings/stage.py
@@ -0,0 +1,25 @@
+from .base import env
+from .production import *  # noqa
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["stage.turbotailer.io"])
+
+ROBOTS = [
+    "User-Agent: *",
+    "Disallow: /",
+]
+
+DEBUG = True
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+
+INTERNAL_IPS = ["128.140.33.147"]
