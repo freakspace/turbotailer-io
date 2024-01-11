49bf30c0bdb9deb52211e75086b76da28f9c848f
Emil Nielsen <hej@emilnielsen.com>
2024-01-11 15:31:37 +0700

    Update translations

--- turbotailer/core/forms.py
+++ turbotailer/core/forms.py
@@ -14,12 +14,12 @@ class AuditForm(forms.ModelForm):
         self.fields["name"].widget.attrs[
             "class"
         ] = "form-input rounded-xl bg-black/5 border-2 border-white/50 shadow-lg placeholder-white text-white w-full"
-        self.fields["name"].widget.attrs["placeholder"] = _("Navn")
+        self.fields["name"].widget.attrs["placeholder"] = _("Name")
         self.fields["website"].widget.attrs[
             "class"
         ] = "form-input rounded-xl bg-black/5 border-2 border-white/50 shadow-lg placeholder-white w-full"
-        self.fields["website"].widget.attrs["placeholder"] = _("Hjemmeside")
+        self.fields["website"].widget.attrs["placeholder"] = _("Website")
         self.fields["email"].widget.attrs[
             "class"
         ] = "form-input rounded-xl bg-black/5 border-2 border-white/50 shadow-lg placeholder-white w-full"
-        self.fields["email"].widget.attrs["placeholder"] = _("Email")
+        self.fields["email"].widget.attrs["placeholder"] = _("E-mail")
